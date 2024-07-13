5fa8596ab1d8f995414b52385ba9356f62c0dce7
dub4 <dub4@aber.ac.uk>
2024-07-13 20:35:54 +0100

    Added Maximied Screen on Launch

--- smart-stock/src/main/java/ddb/App.java
+++ smart-stock/src/main/java/ddb/App.java
@@ -1,20 +1,27 @@
 package ddb;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class App extends Application
 {
     public void start(Stage primaryStage)
     {
-        Label label = new Label("Hello, JavaFX!");
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
         StackPane root = new StackPane();
+        Scene scene = new Scene(root, primaryScreenBounds.getWidth()/2, primaryScreenBounds.getHeight()/2);
+
+        Label label = new Label("Hello, JavaFX!");
         root.getChildren().add(label);
-        Scene scene = new Scene(root, 300, 200);
+
         primaryStage.setTitle("JavaFX Example");
+        primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
