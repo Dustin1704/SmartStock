85c4f16d4c19ca27efca668bdf299d0047e95027
dub4 <dub4@aber.ac.uk>
2024-07-16 17:24:20 +0100

    Added Properties, Constructors and Gets/Sets for ShoppingList Model

--- smart-stock/src/main/java/ddb/models/ShoppingList.java
+++ smart-stock/src/main/java/ddb/models/ShoppingList.java
@@ -1,5 +1,34 @@
 package ddb.models;
 
+import java.util.List;
+
 public class ShoppingList {
+
+    //---------- PROPERTIES ----------//
+    private int id;
+    private List<Item> items;
+
+    //---------- CONSTRUCTORS ----------//
+    /**
+     * Empty ShoppingList Constructor
+     */
+    public ShoppingList(){}
+
+    /**
+     * ShoppingList Constructor
+     */
+    public ShoppingList(int id, List<Item> items){
+        this.id = id;
+        this.items = items;
+    }
+
+    //---------- GETTERS/SETTERS ----------//
+    public int getId(){ return this.id; }
+    public void setId(int id){ this.id = id; }
+
+    public List<Item> getItems(){ return this.items; }
+    public void setItems(List<Item> items){ this.items = items; }
+
+
 
 }
